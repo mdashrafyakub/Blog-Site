8d47be0b843cfd107bf3886f7b580b399b2d6bb2
Your Name <you@example.com>
2022-02-01 18:05:46 +0530

    MongoDB deployment coded added

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ let store =""
 let homeStartingContent1="Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 
 // DB connection
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-ashraf:test123@cluster0.ia5bo.mongodb.net/blogDB", {useNewUrlParser: true});
 const blogSchema = {
   dbContent: String,
   place: String
